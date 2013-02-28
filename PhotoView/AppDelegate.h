4e96502e5881be5984ca242768eba442c25c03e0
ukey <ukey88@gmail.com>
2013-03-01 00:35:51 +0900

    Initial Commit

---
+++ PhotoView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoView
+//
+//  Created by ukey on 2013/03/01.
+//  Copyright (c) 2013年 Yuki Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoView
+//
+//  Created by ukey on 2013/03/01.
+//  Copyright (c) 2013年 Yuki Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
